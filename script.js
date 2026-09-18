858acfe44015c63614865e97fc56b699ae68d821
moskori <soushi0409@gmail.com>
2026-09-18 17:16:55 +0900

    update

--- script.js
+++ script.js
@@ -6,12 +6,4 @@ faqQuestions.forEach((button) => {
 
     item.classList.toggle("is-open");
   });
-});const faqQuestions = document.querySelectorAll(".faq-question");
-
-faqQuestions.forEach((button) => {
-  button.addEventListener("click", () => {
-    const item = button.closest(".faq-item");
-
-    item.classList.toggle("is-open");
-  });
 });
